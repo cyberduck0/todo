d99a405155e2107a3ea89911713276ea0cc9b1ac
Rudolf Sima <rudolf.sima@gmail.com>
2020-03-20 15:12:21 +0100

    Fix mongodb.uri config

--- user-service/app/controllers/UserServiceImpl.scala
+++ user-service/app/controllers/UserServiceImpl.scala
@@ -1,20 +1,14 @@
 package com.fevo.user.service
 
-
-import java.util.UUID
-
 import akka.actor.ActorSystem
 import akka.stream.Materializer
 import cn.playscala.mongo.Mongo
 import com.fevo.user.grpc
 import com.fevo.user.model.Implicits._
 import com.fevo.user.grpc.{AbstractUserServiceRouter, Empty, ListUsersResponse}
-import config.MongoConfig
 import javax.inject.Inject
 import javax.inject.Singleton
 import mongo.model.DbUser
-import play.api.Configuration
-import play.api.mvc.ControllerComponents
 
 import scala.async.Async._
 import scala.concurrent.{ExecutionContext, Future}
