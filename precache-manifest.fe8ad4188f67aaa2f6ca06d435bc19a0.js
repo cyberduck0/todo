0b63cc3a6d5ce17880f2bb9a3d7975ea8b88810c
cyberduck1 <cyberduck1@users.noreply.github.com>
2020-03-23 12:24:22 +0000

    deploy: a95544aeb68f9e9cb7a39b640e25a3ab9fb63121

---
+++ precache-manifest.fe8ad4188f67aaa2f6ca06d435bc19a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77eec25a9e47eb44dcc994b5f26bd7a8",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "39fdf7e3ce4378f5574a",
+    "url": "/todo/static/css/main.b7b12f8c.chunk.css"
+  },
+  {
+    "revision": "42aedab6b164c4ab4773",
+    "url": "/todo/static/js/2.ead3bd66.chunk.js"
+  },
+  {
+    "revision": "37b5fb9fa73e0688add42527cf5813eb",
+    "url": "/todo/static/js/2.ead3bd66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39fdf7e3ce4378f5574a",
+    "url": "/todo/static/js/main.a31d2efe.chunk.js"
+  },
+  {
+    "revision": "81f70e12c9c2f0a743bc",
+    "url": "/todo/static/js/runtime-main.f2e3649c.js"
+  }
+]);
